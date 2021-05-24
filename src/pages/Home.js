694efc593efb8c67e50b700626a55bade96cd7d1
Patrick Motta <patrick.ramos.motta@gmail.com>
2021-05-24 16:57:10 -0300

    adicionado saudaçoes dinamicas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
-import React, { useState }from 'react';
+import React, { useState, useEffect }from 'react';
 
 import {
    View, 
    Text,
    StyleSheet,
    TextInput,
-   Platform, 
+   Platform,
+   ScrollView,
+   FlatList,
 } from 'react-native'
 
 import { Button } from '../components/Button';
@@ -15,19 +17,43 @@ export function Home() {
 
    const [newSkill, setNewSkill] = useState('');
    const [mySkills, setMySkills] = useState([]);
-
+   const [greeting, setGreeting] = useState('');
    function handleAddNewSkill(){
       // No estado tem como recuperar oque esta dentro dele com essa função. Para poder adicionar varias coisas dentro do array
       setMySkills(oldState => [...oldState, newSkill]);
    }
 
 
+   // useEffect(() =>{
+   //    console.log("UseEffect executado")
+
+   // },[mySkills])// nesse caso, toda vez que o mySkills for modigicado, vai executar oque esta dentro do useEffect. Se não tiver nada aqui, so executa quando inicia o app
+
+   
+   useEffect(() =>{
+
+      const currentHour = new Date().getHours();
+      // console.log(currentHour)
+      if(currentHour < 12){
+         setGreeting('Bom dia')
+      }else if(currentHour >= 12 && currentHour < 18){
+         setGreeting('Boa tarde')
+      }else{
+         setGreeting('Boa noite')
+      }
+
+   },[])
+
    return (
 
       <View style={styles.container}>
-         
+      
          <Text style={styles.title}>
             Bem vindo
+         </Text>
+
+         <Text style={styles.greetings}>
+            {greeting}
          </Text>
 
          <TextInput 
@@ -49,13 +75,29 @@ export function Home() {
             Minhas Skills
          </Text>
 
-         {/* as chaves {} são usada assim quando combinamos codigo jsx(Text, TouchOpacity e etc) com javascript */}
+         
+         <FlatList 
+            // Essa e onde esta a "lista" de elementos.
+            data={mySkills}
+            // para colocar a key do elemento e o react native não reclamar
+            keyExtractor={item => item}
+            // Aqui e onde coloca o componente que sera renderizado.
+            renderItem={({ item }) =>(
+               <SkillCard skill={item}/>
+            )}
+            />
+         
+         
+         
+         {/* as chaves {} são usada assim quando combinamos codigo jsx(Text, TouchOpacity e etc) com javascript
          {
             // o map percorre cada elemento dentro do vetor.
             mySkills.map(skill => (
-               <SkillCard skill={skill}/>
+               <SkillCard key={skill} skill={skill}/>
             ))
-         }
+         } */}
+
+         
       </View>
 
    )
@@ -82,5 +124,8 @@ export function Home() {
        marginTop: 30,
        borderRadius: 7
     },
+    greetings: {
+       color: '#fff'
+    }
 
  })
